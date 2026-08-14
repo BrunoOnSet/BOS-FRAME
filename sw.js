2489cd4bee370a3d5faf26a30118ce036684906a
BrunoSetTools <brunoguillardopv@gmail.com>
2026-08-14 21:41:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='frame-bst-v1-8';
+const CACHE='frame-bst-v1-9';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-180.png'];
 
 self.addEventListener('install',e=>{
